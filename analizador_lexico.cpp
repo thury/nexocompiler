335534ebef33be7e0028e35e3bfc8c25145428e5
thuryy <al225658@alumnos.uacj.mx>
2026-09-15 12:53:19 -0600

    linea por token, impl inicial trie para palabras claves

--- analizador_lexico.cpp
+++ analizador_lexico.cpp
@@ -7,12 +7,14 @@
 #include <algorithm>
 #include <fstream>
 #include <sstream>
-#include <cstring> // Necesario para strerror
-#include <cerrno>  // Necesario para errno
+#include <cstring>
+#include <cerrno>
+#include <iomanip> // Necesario para dar formato a la tabla (std::setw, std::left)
 
-
-std::unordered_set<std::string> palabrasClave = { "arranca", "muestra", "capturar",
+const std::unordered_set<std::string> palabrasClave = { "arranca", "muestra", "capturar",
 	 "si", "entonces", "sino", "mientras", "hacer", "cierra" };
+
+std::unordered_set<char> lenguajePalabrasClave;
 
 std::vector<std::vector<int>> matriz;
 
@@ -45,53 +47,100 @@ enum columna {
 struct Token {
 	std::string tipo;
 	std::string lexema;
+	int linea;
 	Token() = default;
-	Token(const std::string &t, const std::string &l) : tipo(t), lexema(l) {}
+	Token(const std::string &t, const std::string &l, int n) : tipo(t), lexema(l), linea(n) {}
+};
+
+struct TrieNode {
+	std::unordered_map<char, TrieNode*> children;
+	bool isEndOfWord;
+
+	TrieNode(bool endOfWord) : isEndOfWord(endOfWord) {}
+};
+
+class Trie {
+	TrieNode root;
+
+	public:
+	void insert(const std::string &word) {
+		TrieNode *node = &root;
+		for (char c : word) {
+			if (node->children.find(c) == node->children.end()) {
+				node->children[c] = new TrieNode(false);
+			}
+			node = node->children[c];
+		}
+		node->isEndOfWord = true;
+	}
+
+	void deleteTrie(TrieNode *node) {
+		for (auto &pair : node->children) {
+			deleteTrie(pair.second);
+		}
+		delete node;
+	}
+
+	TrieNode* getRoot() {
+		return &root;
+	}
+
+	Trie(char c) : root(TrieNode(false)) {}
+
+	~Trie() {
+		deleteTrie(&root);
+	}
 };
 
 void construir_matriz_transicion() {
     std::vector<std::vector<int>> matriztmp = {
         // 0(esp) 1(dig) 2(.)  3(+-) 4(Ee) 5(let) 6(_)  7(op) 8(=)  9(")  10( ( ) 11( ) ) 12( > )
-        {  0,     1,    -5,   500,  7,    7,    -5,   500,  8,    9,    900,    1000,   1100}, // 0: Inicio
-        {100,     1,     2,   100,  4,    -1,   -1,   -1,   -1,   -1,   100,    100,    100 }, // 1: Entero
-        { -2,     3,    -2,   -2,  -2,    -2,   -2,   -2,   -2,   -1,    -2,     -2,     -2 }, // 2: Punto decimal
-        {200,     3,    -2,   200,  4,    -2,   -2,   -1,   -2,  200,   200,    200,    200 }, // 3: Real
+        {  0,     1,    -5,   500,  7,    7,    -5,   500,  8,    9,    900,   1000,    1100}, // 0: Inicio
+        {100,     1,     2,    -3,  4,    -1,   -1,   -1,   -1,   -1,    -1,     -1,     -6 }, // 1: Entero
+        { -2,     3,    -2,    -2, -2,    -2,   -2,   -2,   -2,   -1,    -2,     -2,     -2 }, // 2: Punto decimal
+        {200,     3,    -2,    -1,  4,    -2,   -2,   -1,   -2,   -1,    -1,     -1,     -1 }, // 3: Real
         { -3,     6,    -3,     5, -3,    -3,   -3,   -1,   -2,   -2,    -3,     -3,     -3 }, // 4: 'e' / 'E'
         { -3,     6,    -3,    -3, -3,    -3,   -3,   -1,   -1,   -2,    -3,     -3,     -3 }, // 5: Signo exp
-        {300,     6,    -3,   300, -3,    -3,   -3,   -1,   -1,  300,   300,    300,    300 }, // 6: Cientifico
-        {400,     7,   400,   400,  7,     7,    7,  400,  400,  400,   400,    400,    400 }, // 7: ID 
-        {600,   600,   600,   600, 600,  600,  600,  600,  800,  600,   600,    600,    600 }, // 8: Asignacion (=) o Igualdad (==)
+        {300,     6,    -3,    -4, -4,    -4,   -4,   -4,   -4,   -4,    -4,     -4,     -4 }, // 6: Cientifico
+        {400,     7,    -4,    -6,  7,     7,    7,   -6,   -6,   -6,    -6,     -6,     -6 }, // 7: ID 
+        {600,    -1,    -1,    -1, -1,    -1,   -1,   -1,  800,   -1,    -1,     -6,     -6 }, // 8: Asignacion (=) o Igualdad (==)
         {  9,     9,     9,     9,  9,     9,    9,    9,    9,  700,     9,      9,      9 }  // 9: Cadena ("...")
     };
+
+	// palabras claves
+	Trie trie(' '); // Inicializa el Trie 
+	for (const auto &palabra : palabrasClave) {
+		trie.insert(palabra);
+	}
 
     matriz = matriztmp;
 }
 
 int obtener_columna(char c) {
     switch (c) {
-        case ' ': return DELIMITADOR; // delimitadores
+        case ' ': return DELIMITADOR;
         case '\t': return DELIMITADOR;
         case '\n': return DELIMITADOR;
-        case '.': return PUNTO; // punto
-        case '+': return SIGNO; // signos
+        case '.': return PUNTO;
+        case '+': return SIGNO;
         case '-': return SIGNO;
-        case 'E': return NOTACION_CIENTIFICA; // notacion cientifica
+        case 'E': return NOTACION_CIENTIFICA;
         case 'e': return NOTACION_CIENTIFICA;
-        case '_': return GUION_BAJO; // guion bajo
-        case '*': return OPERADOR; // operadores
+        case '_': return GUION_BAJO;
+        case '*': return OPERADOR;
         case '/': return OPERADOR;
         case '%': return OPERADOR;
-        case '=': return OP_IGUAL; // asignacion
-        case '"': return COMILLAS; // comillas
-        case '(': return PARENTESIS_ABRIR; // parentesis abre
-        case ')': return PARENTESIS_CERRAR; // parentesis cierra
-        case '>': return COMPARADOR_MAYOR; // comparador
+        case '=': return OP_IGUAL;
+        case '"': return COMILLAS;
+        case '(': return PARENTESIS_ABRIR;
+        case ')': return PARENTESIS_CERRAR;
+        case '>': return COMPARADOR_MAYOR;
         default:
             if (std::isdigit(static_cast<unsigned char>(c))) 
-                return 1;
+                return DIGITO;
             if (std::isalpha(static_cast<unsigned char>(c))) 
-                return 5;
-            return -1; // caracter no reconocido
+                return LETRA;
+            return -1;
     } 
     return -1;
 }
@@ -112,6 +161,7 @@ std::string obtener_tipo_token(int q) {
         default: return "Token";
     }
 }
+
 std::string obtener_tipo_lexema(int q, const std::string &lexema) {
 	if (q == 400) {
 		auto lexema_lower = lexema;
@@ -119,17 +169,17 @@ std::string obtener_tipo_lexema(int q, const std::string &lexema) {
 			std::transform(lexema_lower.begin(), lexema_lower.end(), lexema_lower.begin(), [](unsigned char c) {
 				return std::toupper(c);
 			});
-			return "KW_" + lexema_lower; // Palabra clave
+			return "KW_" + lexema_lower;
 		}
 	}
 	return obtener_tipo_token(q);
 }
 
-void error_lexico(const std::string &lexema, int error_code = -1) {
-	std::cout << "Error lexico en: " << lexema << std::endl;
+void error_lexico(const std::string &lexema, int error_code, int linea) {
+	std::cout << "[ERROR LEXICO] En linea " << linea << " | Token: \"" << lexema << "\" -> ";
 	switch (error_code) {
 		case ERROR_FORMATO_NUM_REAL:
-			std::cout << "Error de formato en el numero real o notacion cientifica" << std::endl;
+			std::cout << "Error de formato en el numero real" << std::endl;
 			break;
 		case ERROR_FORMATO_NUM_NOTACION_CIENTIFICA:
 			std::cout << "Error de formato en la notacion cientifica" << std::endl;
@@ -150,7 +200,7 @@ void error_lexico(const std::string &lexema, int error_code = -1) {
 			std::cout << "Error de definicion" << std::endl;
 			break;
 		default:
-			std::cout << "Error desconocido" << std::endl;
+			std::cout << "Caracter no reconocido o invalido" << std::endl;
 			break;
 	}
 }
@@ -159,38 +209,61 @@ std::vector<Token> busca_simbolos(const std::vector<std::vector<int>> &matriz, c
 	std::vector<Token> tokens;
 	int q = 0;
 	std::string lexema_actual;
+	int linea_actual = 1;
 
 	size_t i = 0;
 	while (i < cadena.size()) {
-		int col = obtener_columna(cadena[i]);
+		char c = cadena[i];
 
-		if (col == -1) { // Carácter escaneado no reconocido
+		// Omite los caracteres de salto de línea o retorno de carro sin reportar error léxico
+		if (c == '\n' || c == '\r') {
+			if (c == '\n') {
+				linea_actual++;
+			}
+			if (!lexema_actual.empty()) {
+				int estado_final = matriz[q][DELIMITADOR];
+				if (estado_final != 0 && estado_final % 100 == 0) {
+					tokens.emplace_back(obtener_tipo_lexema(estado_final, lexema_actual), lexema_actual, linea_actual - 1);
+				}
+				q = 0;
+				lexema_actual.clear();
+			}
+			i++;
+			continue;
+		}
+
+		int col = obtener_columna(c);
+
+		if (col == -1) {
 			int estado_final = matriz[q][DELIMITADOR];
 			if (estado_final != 0 && estado_final % 100 == 0) {
-				tokens.emplace_back(obtener_tipo_lexema(estado_final, lexema_actual), lexema_actual);
+				tokens.emplace_back(obtener_tipo_lexema(estado_final, lexema_actual), lexema_actual, linea_actual);
 				q = 0;
 				lexema_actual.clear();
 				continue;
 			}
-			std::string s(1, cadena[i]);
-			error_lexico(s, -100);
+			std::string s(1, c);
+			error_lexico(s, -100, linea_actual);
 			i++;
 			continue;
 		}
 
 		int nuevo_estado = matriz[q][col];
 
-		if (nuevo_estado < 0) { // Error en la transición
+		if (nuevo_estado < 0) {
 			int estado_final = matriz[q][DELIMITADOR];
 			if (estado_final != 0 && estado_final % 100 == 0) {
-				tokens.emplace_back(obtener_tipo_lexema(estado_final, lexema_actual), lexema_actual);
+				tokens.emplace_back(obtener_tipo_lexema(estado_final, lexema_actual), lexema_actual, linea_actual);
 				q = 0;
 				lexema_actual.clear();
 				continue;
 			}
 			if (!lexema_actual.empty()) {
-				std::string l = lexema_actual + cadena[i];
-				error_lexico(l, nuevo_estado);
+				std::string l = lexema_actual + c;
+				error_lexico(l, nuevo_estado, linea_actual);
+			} else {
+				std::string s(1, c);
+				error_lexico(s, nuevo_estado, linea_actual);
 			}
 			q = 0;
 			lexema_actual.clear();
@@ -201,10 +274,10 @@ std::vector<Token> busca_simbolos(const std::vector<std::vector<int>> &matriz, c
 		q = nuevo_estado;
 
 		if (col != 0)
-			lexema_actual.push_back(cadena[i]);
+			lexema_actual.push_back(c);
 
 		if (q % 100 == 0 && q != 0) {
-			tokens.emplace_back(obtener_tipo_lexema(q, lexema_actual), lexema_actual);
+			tokens.emplace_back(obtener_tipo_lexema(q, lexema_actual), lexema_actual, linea_actual);
 			q = 0;
 			lexema_actual.clear();
 		}
@@ -213,14 +286,6 @@ std::vector<Token> busca_simbolos(const std::vector<std::vector<int>> &matriz, c
 	}
 
 	return tokens;
-}
-
-static inline std::string trim(const std::string &s) {
-	size_t start = 0;
-	while (start < s.size() && std::isspace(static_cast<unsigned char>(s[start]))) ++start;
-	size_t end = s.size();
-	while (end > start && std::isspace(static_cast<unsigned char>(s[end-1]))) --end;
-	return s.substr(start, end - start);
 }
 
 std::string leer_archivo(const std::string &nombre_archivo) throw(std::ios_base::failure) {
@@ -239,15 +304,30 @@ int main(int argc, char *argv[]) {
 	std::string cadena;
 	std::string nombre_archivo = argc > 1 ? argv[1] : "ejemplo.nexo";
 
-	cadena = leer_archivo(nombre_archivo);
+	try {
+		cadena = leer_archivo(nombre_archivo);
+	} catch (...) {
+		return 1;
+	}
 	
-	cadena = trim(cadena) + " "; // Agrega un espacio al final para procesar el último token
+	cadena = cadena + " ";
 
 	std::vector<Token> tokens = busca_simbolos(matriz, cadena);
 
+	std::cout << "\n====================================================================\n";
+	std::cout << std::left 
+	          << std::setw(25) << "TOKEN" 
+	          << std::setw(30) << "TIPO DE TOKEN" 
+	          << std::setw(10) << "LINEA" << "\n";
+	std::cout << "====================================================================\n";
+
 	for (const auto &t : tokens) {
-		std::cout << t.lexema << " -> " << t.tipo << std::endl;
+		std::cout << std::left 
+		          << std::setw(25) << t.lexema 
+		          << std::setw(30) << t.tipo 
+		          << std::setw(10) << t.linea << "\n";
 	}
+	std::cout << "====================================================================\n";
 
 	return 0;
 }
